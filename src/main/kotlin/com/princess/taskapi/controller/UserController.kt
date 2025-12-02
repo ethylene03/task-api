ce273f5567ad7287e3956cff6758b4efac6cd5cb
Princess Ethel Atillo <peatillo@vauldex.com>
2025-12-02 15:26:56 +0800

    feat: create user crud methods

---
+++ src/main/kotlin/com/princess/taskapi/controller/UserController.kt
@@ -0,0 +1,44 @@
+package com.princess.taskapi.controller
+
+import com.princess.taskapi.dto.UserDTO
+import com.princess.taskapi.service.UserService
+import jakarta.validation.Valid
+import org.slf4j.LoggerFactory
+import org.springframework.http.HttpStatus
+import org.springframework.validation.annotation.Validated
+import org.springframework.web.bind.annotation.*
+import java.util.*
+
+@Validated
+@RestController
+@RequestMapping("/users")
+class UserController(private val service: UserService) {
+
+    private val log = LoggerFactory.getLogger(this::class.java)
+
+    @ResponseStatus(HttpStatus.CREATED)
+    @PostMapping
+    fun create(@Valid @RequestBody details: UserDTO): UserDTO {
+        log.info("Running POST /users method.")
+        return service.create(details).also { log.info("User created.") }
+    }
+
+    @GetMapping("/{id}")
+    fun find(@PathVariable("id") id: UUID): UserDTO {
+        log.info("Running GET /users/{id} method.")
+        return service.find(id).also { log.info("User fetched.") }
+    }
+
+    @PutMapping("/{id}")
+    fun update(@PathVariable("id") id: UUID, @Valid @RequestBody details: UserDTO): UserDTO {
+        log.info("Running PUT /users/{id} method.")
+        return service.update(id, details).also { log.info("User updated.") }
+    }
+
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @DeleteMapping("/{id}")
+    fun delete(@PathVariable("id") id: UUID) {
+        log.info("Running DELETE /users/{id} method.")
+        return service.delete(id).also { log.info("User deleted.") }
+    }
+}
---
+++ src/main/kotlin/com/princess/taskapi/service/UserService.kt
@@ -0,0 +1,72 @@
+package com.princess.taskapi.service
+
+import com.princess.taskapi.dto.UserDTO
+import com.princess.taskapi.helpers.*
+import com.princess.taskapi.repository.UserRepository
+import org.slf4j.LoggerFactory
+import org.springframework.stereotype.Service
+import java.util.*
+
+@Service
+class UserService(private val repository: UserRepository, private val passwordManager: PasswordManager) {
+    private val log = LoggerFactory.getLogger(this::class.java)
+
+    fun create(user: UserDTO): UserDTO {
+        log.debug("Checking if username is unique..")
+        repository.findByUsername(user.username)
+            ?.let {
+                log.error("Username already exists.")
+                throw DuplicateKeyException("Username already exists.")
+            }
+
+        log.debug("Saving user..")
+        return user.copy(password = passwordManager.hash(user.password))
+            .createUserEntity()
+            .let { repository.save(it) }
+            .toUserResponse()
+    }
+
+    fun find(id: UUID): UserDTO {
+        return repository.findById(id)
+            .orElseThrow {
+                log.error("Data with id $id not found.")
+                throw ResourceNotFoundException("ID does not exist.")
+            }.toUserResponse()
+    }
+
+    fun update(id: UUID, details: UserDTO): UserDTO {
+        log.debug("Checking if username is unique..")
+        repository.findByUsername(details.username)
+            ?.takeIf { it.id == id }
+            ?.let {
+                log.error("Username already exists.")
+                throw DuplicateKeyException("Username already exists.")
+            }
+
+        log.debug("Finding user by given ID..")
+        val currentUser = repository.findById(id)
+            .orElseThrow {
+                log.error("User not found.")
+                ResourceNotFoundException("User not found.")
+            }
+
+        details.password?.takeUnless { passwordManager.isMatch(it, currentUser.password) }
+            ?.run {
+                log.error("Credentials is incorrect.")
+                throw InvalidCredentialsException("Given credentials is incorrect.")
+            }
+
+        return currentUser.apply {
+            name = details.name
+            username = details.username
+        }.run { repository.save(this) }.toUserResponse()
+    }
+
+    fun delete(id: UUID) {
+        log.debug("Checking if ID exists..")
+        find(id)
+
+        log.debug("Deleting data..")
+        repository.deleteById(id)
+    }
+}
